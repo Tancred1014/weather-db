448b59f9bce37af0071714e111bfed9db086a2cb
Tancred1014 <tancred.shih@gmail.com.tw>
2024-08-07 15:02:45 +0800

    router init

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/weathers', (req, res) => {
+  res.send('get all weathers')
+})
+
+app.get('/weathers/:city', (req, res) => {
+  res.send(`get weather: ${req.params.id}`)
+})
+
 app.listen(3000, () => {
   console.log('App is running on post 3000')
 })
